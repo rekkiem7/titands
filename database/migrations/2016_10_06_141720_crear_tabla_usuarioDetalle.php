1ca23eb062cb404c0347a0e6223c6c38b8700784
rekkiem7 <pbarria.reyes@gmail.com>
2016-10-06 17:25:29 +0200

    migratio usuario detalle

---
+++ database/migrations/2016_10_06_141720_crear_tabla_usuarioDetalle.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuarioDetalle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario_detalle', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_usuario');
+            $table->string('nombre1');
+            $table->string('nombre2')->nullable();
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno')->nullable();
+            $table->string('rut');
+            $table->string('sexo');
+            $table->string('direccion')->nullable();
+            $table->string('correo')->nullable();
+            $table->string('telefono')->nullable();
+            $table->string('celular')->nullable();
+            $table->string('avatar')->nullaable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('usuario_detalle');
+    }
+}
